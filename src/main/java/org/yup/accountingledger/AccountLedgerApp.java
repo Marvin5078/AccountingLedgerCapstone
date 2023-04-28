b1b3cbd2c578bfd494f9d0601b9b6549db1bc535
Marvin Oreste <marvin5078@gmail.com>
2023-04-28 15:28:28 -0400

    Git commit

---
+++ src/main/java/org/yup/accountingledger/AccountLedgerApp.java
@@ -0,0 +1,9 @@
+package org.yup.accountingledger;
+
+public class AccountLedgerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("'Welcome to your capstone Marvin!'");
+    }
+}
